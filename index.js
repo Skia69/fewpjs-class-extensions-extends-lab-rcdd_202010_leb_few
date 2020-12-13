39fee48720c379417a5f780986d733c18b6694bb
Mhamad Ibrahim <skiastep@gmail.com>
2020-12-13 20:11:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,10 +31,7 @@ class Square extends Polygon {
 
   get isValid() {
     if (this.count !== 4) return;
-    let side1 = this.sides[0]
-    let side2 = this.sides[1]
-    let side3 = this.sides[2]
-    let side4 = this.sides[3]
+    const [side1, side2, side3, side4] = sides
     return ((side1 === side2) && (side1 === side3) && (side1 === side4))
   }
 
